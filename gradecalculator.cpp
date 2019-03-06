932f1a2ea9959d10dcc02a9dd74310e332925323
Jacqueline Nguyen <beuyenj@gmail.com>
2019-03-06 15:11:18 -0800

    Fixed grade calculation equation

--- gradecalculator.cpp
+++ gradecalculator.cpp
@@ -11,6 +11,24 @@ GradeCalculator::GradeCalculator(QWidget *parent) :
                       this, SLOT( ComputeGrade() )  );
     QObject::connect( ui->HW2Spin,SIGNAL( valueChanged(int) ),
                       this, SLOT( ComputeGrade() ) );
+    QObject::connect( ui->HW3Spin,SIGNAL( valueChanged(int) ),
+                      this, SLOT( ComputeGrade() )  );
+    QObject::connect( ui->HW4Spin,SIGNAL( valueChanged(int) ),
+                      this, SLOT( ComputeGrade() ) );
+    QObject::connect( ui->HW5Spin,SIGNAL( valueChanged(int) ),
+                      this, SLOT( ComputeGrade() )  );
+    QObject::connect( ui->HWSpin6,SIGNAL( valueChanged(int) ),
+                      this, SLOT( ComputeGrade() ) );
+    QObject::connect( ui->HW7Spin,SIGNAL( valueChanged(int) ),
+                      this, SLOT( ComputeGrade() )  );
+    QObject::connect( ui->HW8Spin,SIGNAL( valueChanged(int) ),
+                      this, SLOT( ComputeGrade() ) );
+    QObject::connect( ui->Midterm1spinBox,SIGNAL( valueChanged(int) ),
+                      this, SLOT( ComputeGrade() )  );
+    QObject::connect( ui->Midterm2spinbox,SIGNAL( valueChanged(int) ),
+                      this, SLOT( ComputeGrade() ) );
+    QObject::connect( ui->Finalspinbox,SIGNAL( valueChanged(int) ),
+                      this, SLOT( ComputeGrade() ) );
 
 
 }
@@ -22,9 +40,22 @@ GradeCalculator::~GradeCalculator()
 
 
 void GradeCalculator::ComputeGrade() {
-    double x1 = ui->HW1Spin->value();
-    double x2 = ui->HW2Spin->value();
+    double h1 = ui->HW1Spin->value();
+    double h2 = ui->HW2Spin->value();
+    double h3 = ui->HW3Spin->value();
+    double h4 = ui->HW4Spin->value();
+    double h5 = ui->HW5Spin->value();
+    double h6 = ui->HWSpin6->value();
+    double h7 = ui->HW7Spin->value();
+    double h8 = ui->HW8Spin->value();
 
-    QString text(QString::number( x1 + x2 ));
+    double e1 = ui->Midterm1spinBox->value();
+    double e2 = ui->Midterm2spinbox->value();
+    double e3 = ui->Finalspinbox->value();
+
+    double hw_sum = h1+h2+h3+h4+h5+h6+h7+h8;
+    double overall = .25*(hw_sum/8) + .2*e1 + .2*e2 + .35*e3;
+
+    QString text(QString::number(overall));
     ui->CalculatedGrade->setText(text);
 }
